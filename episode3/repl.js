f26366c4036e95e88c04cd8a3ccdc06b5166288a
Daryassatya <daryassatya@gmail.com>
2022-12-18 13:22:00 +0700

    Episode3 - REPL

---
+++ episode3/repl.js
@@ -0,0 +1,46 @@
+/*
+Episode 3 Membahas tentang REPL (Read, Evel, Print, Loop)
+
+Command Line List : 
+
+1. node
+Digunakan untuk menjalankan nodejs, 
+didalamnya kita dapat menjalankan susuatu seperti yang ada pada terminal web browser
+
+2. gobal
+untuk akses sebuah variabel global
+
+3. global. lalu tab 2x
+Menampilkan modul untuk digunakan
+
+4. .help
+untuk menapilkan special keyword atau command
+
+5. .break
+untuk mengakhiri proses secara paksa pada saat menjalankan atau melakukan sesuatu didalam nodejs
+
+6. .clear
+Alias untuk break
+
+7. .exit
+Untuk keluar dari mode nodejs
+
+8. .load namaFile.js
+Untuk menggunakan isi coding dari suatu file yang dipanggil
+
+9. .save namaFile.js
+Setelah selesai mengetikkan sesuatu di dalam mode nodejs, jika ingin menyimpannya maka gunakanlah
+comman diatas.
+
+10. cat namaFile.js (unix)
+Untuk menampilkan isi file dari file yang dipanggil
+
+11. vi namaFile.js (unix)
+Untuk masuk kedalam kode editor, namun harus masuk kedalam insert mode terlebih dahulu dengan mengetik 
+
+12. rm namaFile.js (unix)
+Berfungsi untuk menghapus file, wajib berhati2 jika ingin menggunakan command ini
+
+13. .editor
+
+*/
